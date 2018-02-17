db2557acb68111376324d276dbbf091fb6391695
cris2301 <36246852+cris2301@users.noreply.github.com>
2018-02-16 22:15:08 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,13 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while (clave!="utn750")
+
+{
+    clave = prompt("error reinicar la clave");
+}
+alert ("clave correcta");
+
+
 
 }//FIN DE LA FUNCIÓN
